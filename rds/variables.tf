a3c6a159b55f1303adb7f3d7f6860470cc9e4929
Silas Teixeira <silas.cloudspace@gmail.com>
2024-09-11 07:39:11 +0100

    created rds module

---
+++ rds/variables.tf
@@ -0,0 +1,7 @@
+# Security groups variable
+variable "project_name" {}
+variable "environment" {}
+variable "vpc_id" {}
+variable "ssh_ip" {}
+variable "db_master_username" {}
+variable "db_master_password" {}
